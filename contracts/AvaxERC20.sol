794fe8fb9097736959094b13bfe94449dc78f32a
Michael Melvin <mjm.neuro@gmail.com>
2023-12-16 21:44:38 -0500

    implmeneted revert on transfer if max wallet amount was exceeded

--- contracts/AvaxERC20.sol
+++ contracts/AvaxERC20.sol
@@ -4,15 +4,24 @@ pragma solidity ^0.8.0;
 import "@thirdweb-dev/contracts/base/ERC20Base.sol";
 
 contract AvaxERC20 is ERC20Base {
-      constructor(
+    private uint256 constant MAX_BPS = 10000;
+    private uint256 immutable MAX_WALLET_CAP_BASIS_POINTS = 200;
+
+    constructor(
         address _defaultAdmin,
         string memory _name,
-        string memory _symbol
-    )
-        ERC20Base(
-            _defaultAdmin,
-            _name,
-            _symbol
-        )
-    {}
+        string memory _symbol,
+        uint256 _walletCapBasisPoints
+    ) ERC20Base(_defaultAdmin, _name, _symbol) {
+        MAX_WALLET_CAP_BASIS_POINTS = _walletCapBasisPoints;
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 amount) internal override {
+        super._beforeTokenTransfer(from, to, amount);
+        uint256 supply = totalSupply();
+        uint256 currentBalance = balanceOf(to);
+        uint256 maxWalletCap = (supply * MAX_WALLET_CAP_BASIS_POINTS) / 10000;
+
+        require(currentBalance + amount <= maxWalletCap, "Transfer exceeds wallet cap");
+    }
 }
